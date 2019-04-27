ca7df43e6d37ed8db2c60d2c48bb40cfcfd63262
Administrator <327118988@qq.com>
2019-04-27 16:22:10 +0800

    update

--- mssql_select_delte.py
+++ mssql_select_delte.py
@@ -1,9 +1,7 @@
 import pymssql
-import datetime
 db=pymssql.connect(server='192.168.81.14', port=1433, user='lh', password='414871250', database='baobiao')
 cur=db.cursor()
-d='insert into nf_rtx (id,name) values ('+datetime.date+',weweeewe'
-cur.execute(d)
+cur.execute('insert into nf_rtx (id,name) values (100682,\'liude2hua\')')
 db.commit()
 r = cur.execute('select * from nf_rtx ')
 r= cur.fetchall()
@@ -15,4 +13,3 @@ db.close()
 #往数据库里增加数据
 #通过一个交互的界面往数据库里增加、删除、修改、查询数据
 #显示出来的时候，尽可能是一行一行显示
-datetime.datetime
